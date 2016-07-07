0ec480261b50341849d834f2b49545a370c3a396
Tim Toupin <tim2pin@gmail.com>
2016-07-07 10:27:07 -0500

    initial commit

---
+++ marvel.js
@@ -0,0 +1,29 @@
+function fetchJSON(url) {
+  return fetch(url).then(function(response) {
+    var contentType = response.headers.get("content-type");
+    if(contentType && contentType.indexOf("application/json") !== -1) {
+      return response.json();
+    } else {
+      console.log("Oops, we haven't got JSON!");
+    }
+  });
+}
+
+function marvelFactory(config) {
+  return function(path) {
+    var timestamp = new Date().getTime();
+    var hash = CryptoJS.MD5(timestamp + config.privateKey + config.publicKey).toString();
+    var url = config.hostname + '/v' + config.version + '/public' + path + '?apikey=' + config.publicKey + '&ts=' + timestamp + '&hash=' + hash;
+    console.log(url);
+
+    return fetchJSON(url);
+  }
+}
+
+// Get an instance of the marvel api
+var marvel = marvelFactory({
+  hostname: 'http://gateway.marvel.com',
+  publicKey: '1e883cd8e8224161c01454a8a00291d4',
+  privateKey: '88effafb5aac6f6ed47faa06d51782c4c8e796f7',
+  version: '1'
+});
